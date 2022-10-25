35ccecbbae2c30026c0827b7a4ae64ce76474f27
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-10-25 15:08:01 +0700

    Add Book - Handle submit HTML form function for adding a book

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const submitForm = document.getElementById('inputBookForm');
+    submitForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        addBook();
+    });
+});
+
+function addBook() {
+    console.log('Function Add Book called');
+}
