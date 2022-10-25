b1b6ba0572cf88e6ea8a80744126004ca81b6d9c
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-10-25 15:36:06 +0700

    Add a Book - Get data from HTML form to add the Book

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const books = [];
+
 document.addEventListener('DOMContentLoaded', function () {
     const submitForm = document.getElementById('inputBookForm');
     submitForm.addEventListener('submit', function (event) {
@@ -7,9 +9,16 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function addBook() {
+    const bookTitle = document.getElementById('inputBookTitle').value;
+    const bookAuthor = document.getElementById('inputBookAuthor').value;
+    const bookYear = document.getElementById('inputBookYear').value;
+    const bookIsComplete = document.getElementById('inputBookIsComplete').checked;
+
     const generatedBookId = generateBookId();
-    console.log('Function Add Book called');
-    console.log('Book id ' + generatedBookId);
+    const bookObject = generateBookObject(generatedBookId, bookTitle, bookAuthor, bookYear, bookIsComplete);
+    books.push(bookObject);
+
+    console.log('Book ' + generatedBookId + ' ' + bookTitle + ' ' + bookAuthor + ' ' + bookYear + ' ' + bookIsComplete);
 }
 
 function generateBookId() {
